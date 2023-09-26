9959f1b5b9f93de9788e409678634791d392604b
miten <miten@MP-PRO-8>
2023-09-26 21:50:27 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ABMTcVLC
+{
+    partial class FormAMBT_VLC
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormAMBT_VLC));
+            this.AxVLC = new Vlc.DotNet.Forms.VlcControl();
+            this.BtnLoad = new System.Windows.Forms.Button();
+            this.BtnStop = new System.Windows.Forms.Button();
+            this.BtnPlayPause = new System.Windows.Forms.Button();
+            this.ChkRepeat = new System.Windows.Forms.CheckBox();
+            this.BtnLoop1 = new System.Windows.Forms.Button();
+            this.BtnLoop2 = new System.Windows.Forms.Button();
+            this.LblLoop = new System.Windows.Forms.Label();
+            this.OpenFileDlg = new System.Windows.Forms.OpenFileDialog();
+            this.Trkbar = new System.Windows.Forms.TrackBar();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.AxVLC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trkbar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AxVLC
+            // 
+            this.AxVLC.BackColor = System.Drawing.Color.Black;
+            this.AxVLC.Location = new System.Drawing.Point(32, 61);
+            this.AxVLC.Margin = new System.Windows.Forms.Padding(4);
+            this.AxVLC.Name = "AxVLC";
+            this.AxVLC.Size = new System.Drawing.Size(819, 404);
+            this.AxVLC.Spu = -1;
+            this.AxVLC.TabIndex = 0;
+            this.AxVLC.Text = "VLCcontrol1";
+            this.AxVLC.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("AxVLC.VlcLibDirectory")));
+            this.AxVLC.VlcMediaplayerOptions = null;
+            this.AxVLC.LengthChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerLengthChangedEventArgs>(this.AxVLC_LengthChanged);
+            this.AxVLC.MediaChanged += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerMediaChangedEventArgs>(this.AxVLC_MediaChanged);
+            this.AxVLC.Playing += new System.EventHandler<Vlc.DotNet.Core.VlcMediaPlayerPlayingEventArgs>(this.AxVLC_Playing);
+            // 
+            // BtnLoad
+            // 
+            this.BtnLoad.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnLoad.Location = new System.Drawing.Point(27, 15);
+            this.BtnLoad.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnLoad.Name = "BtnLoad";
+            this.BtnLoad.Size = new System.Drawing.Size(100, 40);
+            this.BtnLoad.TabIndex = 1;
+            this.BtnLoad.Text = "LOAD";
+            this.BtnLoad.UseVisualStyleBackColor = false;
+            this.BtnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // BtnStop
+            // 
+            this.BtnStop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnStop.Location = new System.Drawing.Point(239, 15);
+            this.BtnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnStop.Name = "BtnStop";
+            this.BtnStop.Size = new System.Drawing.Size(100, 40);
+            this.BtnStop.TabIndex = 3;
+            this.BtnStop.Text = "Stop";
+            this.BtnStop.UseVisualStyleBackColor = false;
+            this.BtnStop.Click += new System.EventHandler(this.BtnStop_Click);
+            // 
+            // BtnPlayPause
+            // 
+            this.BtnPlayPause.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnPlayPause.Location = new System.Drawing.Point(134, 15);
+            this.BtnPlayPause.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnPlayPause.Name = "BtnPlayPause";
+            this.BtnPlayPause.Size = new System.Drawing.Size(100, 40);
+            this.BtnPlayPause.TabIndex = 4;
+            this.BtnPlayPause.Text = "Play";
+            this.BtnPlayPause.UseVisualStyleBackColor = false;
+            this.BtnPlayPause.Click += new System.EventHandler(this.BtnPlayPause_Click);
+            // 
+            // ChkRepeat
+            // 
+            this.ChkRepeat.AutoSize = true;
+            this.ChkRepeat.Location = new System.Drawing.Point(467, 15);
+            this.ChkRepeat.Margin = new System.Windows.Forms.Padding(4);
+            this.ChkRepeat.Name = "ChkRepeat";
+            this.ChkRepeat.Size = new System.Drawing.Size(167, 29);
+            this.ChkRepeat.TabIndex = 5;
+            this.ChkRepeat.Text = "Repeat Loop";
+            this.ChkRepeat.UseVisualStyleBackColor = true;
+            this.ChkRepeat.CheckedChanged += new System.EventHandler(this.ChkRepeat_CheckedChanged);
+            // 
+            // BtnLoop1
+            // 
+            this.BtnLoop1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnLoop1.Location = new System.Drawing.Point(633, 15);
+            this.BtnLoop1.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnLoop1.Name = "BtnLoop1";
+            this.BtnLoop1.Size = new System.Drawing.Size(100, 40);
+            this.BtnLoop1.TabIndex = 6;
+            this.BtnLoop1.Text = "10-20";
+            this.BtnLoop1.UseVisualStyleBackColor = false;
+            this.BtnLoop1.Click += new System.EventHandler(this.BtnLoop1_Click);
+            // 
+            // BtnLoop2
+            // 
+            this.BtnLoop2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BtnLoop2.Location = new System.Drawing.Point(751, 15);
+            this.BtnLoop2.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnLoop2.Name = "BtnLoop2";
+            this.BtnLoop2.Size = new System.Drawing.Size(100, 40);
+            this.BtnLoop2.TabIndex = 7;
+            this.BtnLoop2.Text = "50-80";
+            this.BtnLoop2.UseVisualStyleBackColor = false;
+            this.BtnLoop2.Click += new System.EventHandler(this.BtnLoop2_Click);
+            // 
+            // LblLoop
+            // 
+            this.LblLoop.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.LblLoop.Location = new System.Drawing.Point(17, 601);
+            this.LblLoop.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.LblLoop.Name = "LblLoop";
+            this.LblLoop.Size = new System.Drawing.Size(418, 44);
+            this.LblLoop.TabIndex = 8;
+            // 
+            // Trkbar
+            // 
+            this.Trkbar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Trkbar.Location = new System.Drawing.Point(27, 483);
+            this.Trkbar.Name = "Trkbar";
+            this.Trkbar.Size = new System.Drawing.Size(824, 90);
+            this.Trkbar.TabIndex = 9;
+            // 
+            // Timer1
+            // 
+            this.Timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // FormAMBT_VLC
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 672);
+            this.Controls.Add(this.Trkbar);
+            this.Controls.Add(this.LblLoop);
+            this.Controls.Add(this.BtnLoop2);
+            this.Controls.Add(this.BtnLoop1);
+            this.Controls.Add(this.ChkRepeat);
+            this.Controls.Add(this.BtnPlayPause);
+            this.Controls.Add(this.BtnStop);
+            this.Controls.Add(this.BtnLoad);
+            this.Controls.Add(this.AxVLC);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "FormAMBT_VLC";
+            this.Text = "ABMT VLC";
+            ((System.ComponentModel.ISupportInitialize)(this.AxVLC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Trkbar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Vlc.DotNet.Forms.VlcControl AxVLC;
+        private System.Windows.Forms.Button BtnLoad;
+        private System.Windows.Forms.Button BtnStop;
+        private System.Windows.Forms.Button BtnPlayPause;
+        private System.Windows.Forms.CheckBox ChkRepeat;
+        private System.Windows.Forms.Button BtnLoop1;
+        private System.Windows.Forms.Button BtnLoop2;
+        private System.Windows.Forms.Label LblLoop;
+        private System.Windows.Forms.OpenFileDialog OpenFileDlg;
+        private System.Windows.Forms.TrackBar Trkbar;
+        private System.Windows.Forms.Timer Timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Vlc;
+using Vlc.DotNet;
+using Vlc.DotNet.Core;
+using Vlc.DotNet.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using InvokersLib;
+
+namespace ABMTcVLC
+{
+    public partial class FormAMBT_VLC : Form
+    {
+        public string szMedia;
+        // Define a CancellationTokenSource at the class level
+        public CancellationTokenSource cancellationTokenSource;
+        public bool isLooping;
+        public bool isRepeating;
+        public bool isPlaying;
+
+        // https://www.youtube.com/watch?v=iGowt4-nQEM
+
+        public FormAMBT_VLC()
+        {
+            InitializeComponent();
+            szMedia = "C:\\Data\\test-app\\Jhinjoti-vilambit-gat-learn.mp4";
+            AxVLC.Show();
+            Timer1.Interval = 10;
+            // Subscribe to the media duration changed event
+            AxVLC.VlcMediaPlayer.TimeChanged += VlcMediaPlayer_TimeChanged;
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            if (OpenFileDlg.ShowDialog() == DialogResult.OK)
+            {
+                szMedia = OpenFileDlg.FileName;
+                AxVLC.SetMedia(new Uri(szMedia));
+
+                //AxVLC.Play(new Uri(szMedia));
+                if (File.Exists("G:\\FILE_LATER\\ABMusicTrainer\\" + Path.GetFileName(szMedia) + ".loop"))
+                {
+                    LblLoop.Text = "Loop file exists";
+                }
+                else
+                {
+                    LblLoop.Text = " **NO Loop file";
+                }
+            }
+        }
+
+        private void VlcMediaPlayer_TimeChanged(object sender, VlcMediaPlayerTimeChangedEventArgs e)
+        {
+        }
+
+        private void BtnStop_Click(object sender, EventArgs e)
+        {
+            AxVLC.Stop();
+            isPlaying = false;
+            Timer1.Stop();
+        }
+
+        private void BtnPlayPause_Click(object sender, EventArgs e)
+        {
+            if (BtnPlayPause.Text == "Pause")
+            {
+                AxVLC.Pause();
+                isPlaying = true;
+                Timer1.Start();
+                BtnPlayPause.Text = "Play";
+            }
+            else
+            {
+                AxVLC.Play();
+                isPlaying = false;
+                Timer1.Stop();
+                BtnPlayPause.Text = "Pause";
+            }
+        }
+
+        private void BtnLoop1_Click(object sender, EventArgs e)
+        {
+            DoLoop(10 * 1000, 20 * 1000);
+        }
+
+        private void BtnLoop2_Click(object sender, EventArgs e)
+        {
+            DoLoop(50 * 1000, 80 * 1000);
+        }
+
+        private void ChkRepeat_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void DoLoop(int tStart, int tEnd)
+        {
+            // Stop if playing or in a loop
+            //doStop();
+            
+            AxVLC.Time = tStart;
+            AxVLC.Play();
+            isPlaying = true;
+
+            // Create a new CancellationTokenSource
+            cancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await Task.Delay((tEnd - tStart), cancellationTokenSource.Token);
+                //if isRepeating then
+                AxVLC.Stop();
+            }
+            catch (TaskCanceledException)
+            {
+                // This will be thrown if the CancellationToken is cancelled before the delay completes.
+                // You can ignore this or handle it as needed.
+            }
+            isLooping = true;
+        }
+
+//        private void DoStop() // stops the player
+//        {
+//            try
+//            {
+//                cancellationTokenSource?.Cancel();
+//            }
+//            catch { }
+//            AxVLC.Stop();
+//            isLooping = false;
+//            isRepeating = false;
+//        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            if (AxVLC.State == Vlc.DotNet.Core.Interops.Signatures.MediaStates.Playing)
+            {
+                INVOKERS.InvokeTrackerValue(Trkbar, (int)AxVLC.Time / Timer1.Interval); // Convert milliseconds to seconds
+            }
+        }
+
+        private void AxVLC_Playing(object sender, VlcMediaPlayerPlayingEventArgs e)
+        {
+        }
+
+        private void AxVLC_LengthChanged(object sender, VlcMediaPlayerLengthChangedEventArgs e)
+        {
+            // Get the media duration in milliseconds
+            long durationInMilliseconds = AxVLC.VlcMediaPlayer.Length;
+
+            // Convert duration to seconds
+            int durationInSeconds = (int)(durationInMilliseconds / Timer1.Interval);
+            INVOKERS.InvokeTrackerMaximum(Trkbar, (int)durationInSeconds);
+            //Trkbar.Maximum = durationInSeconds;
+        }
+
+        private void AxVLC_MediaChanged(object sender, VlcMediaPlayerMediaChangedEventArgs e)
+        {
+        }
+    }
+}
